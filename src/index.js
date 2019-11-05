2d06ccf8934222760a6d9d6a733def029748bad9
Denis Hasanenko <denis.hasanenko@gmail.com>
2019-11-06 00:01:04 +0200

    Add postman collection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
+const mergeDeep = require('./utils');
 require('dotenv').config();
 
 let typeDefs = [
@@ -13,38 +14,6 @@ let resolvers = {
 resolvers = mergeDeep(resolvers, require('./base/resolvers'),
     require('./users/resolvers'),
     require('./boards/resolvers'));
-
-/**
- * Simple object check.
- * @param item
- * @returns {boolean}
- */
-function isObject(item) {
-    return (item && typeof item === 'object' && !Array.isArray(item));
-}
-
-/**
- * Deep merge two objects.
- * @param target
- * @param ...sources
- */
-function mergeDeep(target, ...sources) {
-    if (!sources.length) return target;
-    const source = sources.shift();
-
-    if (isObject(target) && isObject(source)) {
-        for (const key in source) {
-            if (isObject(source[key])) {
-                if (!target[key]) Object.assign(target, { [key]: {} });
-                mergeDeep(target[key], source[key]);
-            } else {
-                Object.assign(target, { [key]: source[key] });
-            }
-        }
-    }
-
-    return mergeDeep(target, ...sources);
-}
 
 const app = express();
 
